588de3c303e1deb0ce464bb698ad8f77f278b7b7
rsuski <robert.suski@acxiom.com>
2018-10-15 09:32:17 +0200

    Minor updates

--- 6/CEPIK.py
+++ 6/CEPIK.py
@@ -10,18 +10,19 @@ def list_database():
         for f in file.readlines():
                 print(f)
 
-def get_user_database_by_name(mode):
+def get_database_entry(mode):
     found=False
-    name=input("Give name:\n ")
+    key = input("Key to search:\n")
+    value = input("Value to search:\n")
     with open("src/db.dat","+r", encoding="utf-8") as file:
         for f in file.readlines():
+            dbname=eval(f)[key]
             if mode == "i":
-                dbname=eval(f)["name"]
-                if dbname.upper() == name.upper():
+                if dbname.upper() == value.upper():
                     print(f)
                     found=True
             else:
-                if eval(f)["name"] == name:
+                if dbname == value:
                     print(f)
                     found=True
     if not found:
@@ -35,16 +36,17 @@ def add_database_entry():
     print("Entry {} has been added".format(entry))
 
 def remove_database_entry_by_name(mode):
-    name=input("Give name:\n ")
+    key = input("Key to search:\n")
+    value = input("Value to search:\n")
     newdb= open("src/newdb.dat","+w", encoding="utf-8");
     with open("src/db.dat","+r", encoding="utf-8") as file:
         for f in file.readlines():
+            dbname=eval(f)[key]
             if mode == "i":
-                dbname=eval(f)["name"]
-                if dbname.upper() != name.upper():
+                if dbname.upper() != value.upper():
                     newdb.write(f)
             else:
-                if eval(f)["name"] != name:
+                if dbname != value:
                     newdb.write(f)
     newdb.close();
     try:
@@ -53,6 +55,21 @@ def remove_database_entry_by_name(mode):
         pass
     os.rename("src/db.dat","src/db.old")
     os.rename("src/newdb.dat","src/db.dat")
+
+def count_entries(mode):
+    key = input("Key to search:\n")
+    value = input("Value to search:\n")
+    count=0
+    with open("src/db.dat","+r", encoding="utf-8") as file:
+        for f in file.readlines():
+            dbname=eval(f)[key]
+            if mode == "i":
+                if dbname.upper() == value.upper():
+                    count=count+1
+            else:
+                if dbname == value:
+                    count=count+1
+    print("Number of entries: ({}={}) is {}".format(key, value, count))
 
 
 def clear_database():
@@ -65,24 +82,29 @@ while True:
     operation=input("Choose option:\n"
                     "l  - list database\n"
                     "a  - add entry\n"
-                    "sn(i) - search by name, case insensitive when i provided\n"
-                    "c  - clear database\n"
-                    "rn(i)  - remove entry by name, case insensitive when i provided\n"
+                    "s(i) - search, case insensitive when i provided\n"
+                    "r(i)  - remove entry by name, case insensitive when i provided\n"
+                    "c(i)  - count by key and value, case insensitive when i provided\n"
+                    "d  - clear database\n"
                     "q  - exit\n ")
 
     if operation == "l":
         list_database()
     elif operation == "a":
         add_database_entry()
-    elif operation == "sn":
-        get_user_database_by_name("s")
-    elif operation == "sni":
-        get_user_database_by_name("i")
-    elif operation == "rn":
+    elif operation == "s":
+        get_database_entry("s")
+    elif operation == "si":
+        get_database_entry("i")
+    elif operation == "r":
         remove_database_entry_by_name("s")
-    elif operation == "rni":
+    elif operation == "ri":
         remove_database_entry_by_name("i")
     elif operation == "c":
+        count_entries("s")
+    elif operation == "ci":
+        count_entries("i")
+    elif operation == "d":
         clear_database()
     elif operation =="q":
         print("Exiting")
